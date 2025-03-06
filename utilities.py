f78c39c1e9d5b6a368dd3d42b940095b27f2aefb
Matteo PERON <83073774+MatPeron@users.noreply.github.com>
2025-03-06 16:55:10 +0100

    Major update to utility functions
    function that are added as attribute to endow should now be classes, as simple functions cannot handle multiple endow instantiations on different functions (all the functionscalls will point to the latest instantiation). Example:
    
    ```
    f1 = endow(f1, attr=utility_func)
    f2 = endow(f2, attr=utility_func)
    
    f1.utility_func(*args, **kwargs) # actually calls f2.utility_func...
    ```
    
    Now classes `BaseMethod` and `Bundle` have been added, which set up a parent class for utility functions and a way to deliver utility methods together with their args and kwargs to `endow`. `debug` has now been modified to report input and toggle printing of arrays

--- utilities.py
+++ utilities.py
@@ -1,6 +1,3 @@
-
-
-
 #region: endow
 # # FUNCTION WRAPPING UTILITY: ENDOW
 #
@@ -49,22 +46,43 @@
 # code with minimal adjustments while still being readable (just call 
 # `f=endow(f, attr1, ...)` and change the desired calls to the function as 
 # `f -> f.attr1`)
+
 def endow(function, **attributes):
 
     def wrapper(*args, **kwargs):
         return function(*args, **kwargs)
 
-    wrapper.__wrapped_name__ = function.__name__
-
     for attribute, value in attributes.items():
-        setattr(wrapper, attribute, value)
-        
-        if callable(value):
-            value.callback = wrapper
+        if type(value) is Bundle:
+            instance = value.method(
+                wrapper,
+                function.__name__,
+                *value.args,
+                **value.kwargs
+            )
+            setattr(wrapper, attribute, instance.main)
+        elif callable(value):
+            instance = value(wrapper, function.__name__)
+            setattr(wrapper, attribute, instance.main)
+        else:
+            setattr(wrapper, attribute, value)
     
     return wrapper
 
 #endregion
+
+class Bundle:
+
+    def __init__(self, method, *args, **kwargs):
+        self.method = method
+        self.args = args,
+        self.kwargs = kwargs
+
+class BaseMethod:
+
+    def __init__(self, wrapper, wrapped_name):
+        self.callback = wrapper
+        self.__wrapped_name__ = wrapped_name
 
 #region: utility functions
 
@@ -90,55 +108,112 @@ def endow(function, **attributes):
 # ) 
 # ```
 
-import inspect, time, sys, traceback
+import inspect, time, sys, traceback, numpy, os
 
-def get_caller_info():
-    # Get the current frame (the frame of this function)
-    current_frame = inspect.currentframe()
-    
-    # Get the caller's frame (the frame of the function that called debug)
-    caller_frame = current_frame.f_back.f_back
-    
-    # Get the filename and line number from the caller's frame
-    filename = caller_frame.f_code.co_filename
-    line_number = caller_frame.f_lineno
-    
-    # Clean up the frame to avoid reference cycles
-    del current_frame
-    del caller_frame
-    
-    return filename, line_number
-def debug(*args, debug_msg=None, log_file=None, **kwargs):
+class debug(BaseMethod):
 
-    filename, line_number = get_caller_info()
-    print(
-        f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] calling"
-        f" {debug.callback.__wrapped_name__} from file:"
-        f" {filename}, line: {line_number}...",
-        end=" ",
-        file=open(log_file, "a") if log_file is not None else sys.stdout
-    )
-    ti = time.time()
-    try:
-        out = debug.callback(*args, **kwargs)
-        dt = time.time()-ti
-        h, m, s = dt//3600, (dt-dt//3600)//60, dt-(dt-dt//3600)//60
+    def __init__(self, wrapper, wrapped_name, log_file=None, no_arrays=False):
+        super().__init__(wrapper, wrapped_name)
+        self.log_file = log_file
+        self.no_arrays = no_arrays
+        self.reset_when_lines = 10000
+
+    def main(self, *args, debug_msg=None, **kwargs):
+
+        self.reset_maybe()
+
+        filename, line_number = self.get_caller_info()
+        
         print(
-            f"DONE in {h:.0f}h{m:.0f}m{s:.4f}s" + \
-            f"\n  [OUTPUT] {out}" + \
-            int(0 if debug_msg is None else 1)*f"\n  [INFO] {debug_msg}",
-            file=open(log_file, "a") if log_file is not None else sys.stdout
+            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] calling"
+            f" {self.__wrapped_name__} from file:"
+            f" {filename}, line: {line_number}...",
+            file=open(self.log_file, "a") if self.log_file is not None else sys.stdout
         )
+        ti = time.time()
+        
+        if self.no_arrays:
+            if numpy.__version__[0]=="1":
+                numpy.set_string_function(lambda x: "array",  repr=True)
+                numpy.set_string_function(lambda x: "array",  repr=False)
+            elif numpy.__version__[:3]=="2.0":
+                # arrays cannot be completely redacted, try to shorten them as much as possible
+                numpy.set_printoptions(
+                    threshold=3,
+                    edgeitems=1,
+                    formatter={"all": lambda x: ""}
+                )
+            else:
+                numpy.set_printoptions(override_repr=lambda x: "array")
+        
+        try:
+            out = self.callback(*args, **kwargs)
+            dt = time.time()-ti
+            h, m, s = dt//3600, (dt-dt//3600)//60, dt-(dt-dt//3600)//60
+            print(
+                f"  DONE in {h:.0f}h{m:.0f}m{s:.4f}s" + \
+                f"\n  [INPUT] {args} {kwargs}" + \
+                f"\n  [OUTPUT] {out}" + \
+                int(0 if debug_msg is None else 1)*f"\n  [INFO] {debug_msg}",
+                file=open(self.log_file, "a") if self.log_file is not None else sys.stdout
+            )
 
-        return out
-    except Exception as e:
-        print(
-            f"FAILED, error message:\n",
-            file=open(log_file, "a") if log_file is not None else sys.stdout
-        )
-        traceback.print_exc(
-            file=open(log_file, "a") if log_file is not None else sys.stdout
-        )
+            return out
+        except Exception as e:
+            print(
+                f"FAILED, error message:\n",
+                file=open(self.log_file, "a") if self.log_file is not None else sys.stdout
+            )
+            traceback.print_exc(
+                file=open(self.log_file, "a") if self.log_file is not None else sys.stdout
+            )
+
+        if self.no_arrays:
+            if numpy.__version__[0]=="1":
+                numpy.set_string_function(None)
+            elif numpy.__version__[:3]=="2.0":
+                numpy.set_printoptions(
+                    edgeitems=3,
+                    infstr='inf',
+                    linewidth=75,
+                    nanstr='nan',
+                    precision=8,
+                    suppress=False,
+                    threshold=1000,
+                    formatter=None
+                )
+            else:
+                numpy.set_printoptions()
+
+    @staticmethod
+    def get_caller_info():
+        # Get the current frame (the frame of this function)
+        current_frame = inspect.currentframe()
+        
+        # Get the caller's frame (the frame of the function that called debug)
+        caller_frame = current_frame.f_back.f_back
+        
+        # Get the filename and line number from the caller's frame
+        filename = caller_frame.f_code.co_filename
+        line_number = caller_frame.f_lineno
+        
+        # Clean up the frame to avoid reference cycles
+        del current_frame
+        del caller_frame
+        
+        return filename, line_number
+    
+    def reset_maybe(self):
+        
+        if os.path.exists(self.log_file):
+            with open(self.log_file, "r") as f:
+                lines = 0
+                for _ in f:
+                    lines += 1
+                
+            if lines>=self.reset_when_lines:
+                os.remove(self.log_file)
+
 
 # # REMEMBER
 #
@@ -190,7 +265,7 @@ def test():
 
         return sum
 
-    fnew = endow(f, debug=debug, remember=remember)
+    fnew = endow(f, debug=debug, remember=remember);print(dir(fnew.debug));quit()
     iterator = range(0, 10**8)
     factor = 2
 
